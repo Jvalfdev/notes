cb39ce49960cc4dac651c9093b32ad26bdfebce7
Jose Vallejo <73082469+Jvalfdev@users.noreply.github.com>
2022-06-18 09:16:34 +0200

    18/6/22

--- JavaScript/syntax.js
+++ JavaScript/syntax.js
@@ -1,1 +1,23 @@
+//Variables
+var name = " jose";
+var age = 32;
+
+//Arrays
+var namearray = ["Jose", "Mel", "Ad"];
+
+//Conditional
+if (age>18)
+{
+    alert("You are old");
+}
+else
+{
+    alert("You are young");
+}
+
+//Loops
+for (var i = 0; i < 10; i++)
+{
+    alert("This is the " + i + " step");
+}
 
